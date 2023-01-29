5b1488c9e1e2597bdd35b1690c33fbb92b03b1c5
Zac Clay <zacclay@gmail.com>
2023-01-29 15:53:36 -0500

    Exercise 1: GuessInput❓

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+function GuessInput() {
+  const [guess, setGuess] = React.useState("");
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (guess.length !== 5) {
+      window.alert('Guess must be exactly 5 letters long.')
+      return;
+    }
+    console.log({guess});
+    setGuess('');
+  };
+
+  const setValidityMessage = e => {
+    if (e.target.validity.patternMismatch) {
+      e.target.setCustomValidity("Guess must be exactly 5 letters long.");
+    } else {
+      e.target.setCustomValidity("");
+    }
+  };
+
+  return(
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        maxLength={5}
+        minLength={5}
+        pattern="[a-zA-Z]{5}"
+        type="text"
+        value={guess}
+        required={true}
+        onChange={(e) => {
+          setValidityMessage(e);
+          const newGuess = e.target.value.toUpperCase();
+          setGuess(newGuess);
+        }}
+      />
+    </form>
+  )
+}
+
+export default GuessInput;
